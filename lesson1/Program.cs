d8fffb0a5f77a4f5945f989fe68ffe307cfd1802
Makarova Ekaterina <makarova.ekaterina-1410@yandex.ru>
2022-09-02 17:56:18 +0300

    info about lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿// на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+
+if (secondNumber > firstNumber)
+{
+    Console.WriteLine(secondNumber);
+}
+
+
+
