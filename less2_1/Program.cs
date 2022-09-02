dbe83443177913cc197fce3e8cf160cb41b13d2f
Makarova Ekaterina <makarova.ekaterina-1410@yandex.ru>
2022-09-02 18:39:18 +0300

    added info about less 2 1

---
+++ less2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// принимает на вход три числа и выдаёт максимальное из этих чисел. Вариант 2
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+
+if(firstNumber < secondNumber)
+{
+    max=secondNumber;
+}
+if(max < thirdNumber)
+{
+    Console.WriteLine(thirdNumber);
+}
+else 
+{
+    Console.WriteLine(max);
+}
+
