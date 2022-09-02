02cad91fd24bce3e969dd9d82af302c3e33f3882
Makarova Ekaterina <makarova.ekaterina-1410@yandex.ru>
2022-09-02 18:42:38 +0300

    added info about lesson 3

---
+++ lesson3/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+        Console.WriteLine("Нечетное число");
+}
+
+
