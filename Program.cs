75b775d8e5347f0ed3d6b7437036133314693f57
Makarova Ekaterina <makarova.ekaterina-1410@yandex.ru>
2022-09-04 22:40:20 +0300

    added info about dayNumber

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7 :");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void WorkHoliday(int dayNumber)
+{
+    if ( dayNumber > 0 && dayNumber < 8 )
+     
+         
+        if ( dayNumber == 6 || dayNumber == 7 )
+        {
+            Console.WriteLine("Этот день выходной");
+
+        }
+        else
+        {
+            Console.WriteLine("Этот день рабочий");
+        }
+}
+WorkHoliday(dayNumber);
+
+
